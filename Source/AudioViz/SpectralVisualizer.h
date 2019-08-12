ee47eda71bcc2fd3aa5250ef41a92c49afa68878
Kyle Geske <stungeye@gmail.com>
2019-08-12 06:50:24 -0500

    Initial commit with gitignore and clang-format config files in place.

---
+++ Source/AudioViz/SpectralVisualizer.h
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SpectralVisualizer.generated.h"
+
+UCLASS()
+class AUDIOVIZ_API ASpectralVisualizer : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ASpectralVisualizer();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
+
+	void Refresh();
+
+	UPROPERTY(EditAnywhere, Category = "Audio")
+	class UTimeSynthComponent* TimeSynthComponent;
+
+	UPROPERTY(EditAnywhere, Category = "Audio")
+	class UTimeSynthClip* TimeSynthClip;
+
+	UPROPERTY(EditAnywhere, Category = "Audio")
+	TArray<UStaticMeshComponent*> SpectrumBars;
+
+	UPROPERTY(EditAnywhere, Category = "Audio")
+	UStaticMesh* SpectrumBarMesh;
+
+	UPROPERTY(EditAnywhere, Category = "Audio")
+	float SpectrumBarSpacing;
+
+	UPROPERTY(EditAnywhere, Category = "Audio")
+	float InterpolationSpeed;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
