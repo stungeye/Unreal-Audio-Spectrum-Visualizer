093c8a201f553949403c018958e843fb17c58bec
Kyle Geske <stungeye@gmail.com>
2019-08-12 07:01:06 -0500

    After running auto format based on my new clang format config.

--- Source/AudioViz/SpectralVisualizer.cpp
+++ Source/AudioViz/SpectralVisualizer.cpp
@@ -6,70 +6,61 @@
 #include "Components/StaticMeshComponent.h"
 
 // Sets default values
-ASpectralVisualizer::ASpectralVisualizer()
-{
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+ASpectralVisualizer::ASpectralVisualizer() {
+  // Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+  PrimaryActorTick.bCanEverTick = true;
 
-	SpectrumBarSpacing = 100.f;
-	InterpolationSpeed = 5.f;
-	TimeSynthComponent = CreateDefaultSubobject<UTimeSynthComponent>("TimeSynthComponent");
-	TimeSynthComponent->bEnableSpectralAnalysis = true;
+  SpectrumBarSpacing = 100.f;
+  InterpolationSpeed = 5.f;
+  TimeSynthComponent = CreateDefaultSubobject<UTimeSynthComponent>("TimeSynthComponent");
 
-	for(int32 i = 1; i <= 20; ++i)
-	{
-		TimeSynthComponent->FrequenciesToAnalyze.Add(i * 50);
+  TimeSynthComponent->bEnableSpectralAnalysis = true;
 
-		FString SpectrumBarName = FString::Printf(TEXT("SpectrumBar%dHz"), i * 50);
+  for (int32 i = 1; i <= 20; ++i) {
+    TimeSynthComponent->FrequenciesToAnalyze.Add(i * 50);
 
-		UStaticMeshComponent* SpectrumBar = CreateDefaultSubobject<UStaticMeshComponent>(*SpectrumBarName);
-		SpectrumBar->SetupAttachment(TimeSynthComponent);
-		SpectrumBar->SetStaticMesh(SpectrumBarMesh);
-		SpectrumBar->SetRelativeLocation(FVector(i * SpectrumBarSpacing, 0.f, 0.f));
-		SpectrumBars.Add(SpectrumBar);
-	}
+    FString SpectrumBarName = FString::Printf(TEXT("SpectrumBar%dHz"), i * 50);
+
+    UStaticMeshComponent *SpectrumBar = CreateDefaultSubobject<UStaticMeshComponent>(*SpectrumBarName);
+    SpectrumBar->SetupAttachment(TimeSynthComponent);
+    SpectrumBar->SetStaticMesh(SpectrumBarMesh);
+    SpectrumBar->SetRelativeLocation(FVector(i * SpectrumBarSpacing, 0.f, 0.f));
+    SpectrumBars.Add(SpectrumBar);
+  }
 }
 
 // Called when the game starts or when spawned
-void ASpectralVisualizer::BeginPlay()
-{
-	Super::BeginPlay();
-	Refresh();
+void ASpectralVisualizer::BeginPlay() {
+  Super::BeginPlay();
+  Refresh();
 
-	TimeSynthComponent->PlayClip(TimeSynthClip);
+  TimeSynthComponent->PlayClip(TimeSynthClip);
 }
 
-void ASpectralVisualizer::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
-{
-	Super::PostEditChangeProperty(PropertyChangedEvent);
-	Refresh();
+void ASpectralVisualizer::PostEditChangeProperty(FPropertyChangedEvent &PropertyChangedEvent) {
+  Super::PostEditChangeProperty(PropertyChangedEvent);
+  Refresh();
 }
 
-void ASpectralVisualizer::Refresh()
-{
-	for (int32 i = 0; i < 20; ++i)
-	{
-		if (SpectrumBars.IsValidIndex(i)) {
-			UStaticMeshComponent* SpectrumBar = SpectrumBars[i];
-			SpectrumBar->SetStaticMesh(SpectrumBarMesh);
-			SpectrumBar->SetRelativeLocation(FVector((i+1) * SpectrumBarSpacing, 0.f, 0.f));
-		}
-	}
+void ASpectralVisualizer::Refresh() {
+  for (int32 i = 0; i < 20; ++i) {
+    if (SpectrumBars.IsValidIndex(i)) {
+      UStaticMeshComponent *SpectrumBar = SpectrumBars[i];
+      SpectrumBar->SetStaticMesh(SpectrumBarMesh);
+      SpectrumBar->SetRelativeLocation(FVector((i + 1) * SpectrumBarSpacing, 0.f, 0.f));
+    }
+  }
 }
-
 
 
 // Called every frame
-void ASpectralVisualizer::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
+void ASpectralVisualizer::Tick(float DeltaTime) {
+  Super::Tick(DeltaTime);
 
-	for(auto SpecData: TimeSynthComponent->GetSpectralData())
-	{
-		UStaticMeshComponent* SpectrumBar = SpectrumBars[SpecData.FrequencyHz / 50.f - 1];
-		FVector BarScale = SpectrumBar->GetComponentScale();
-		BarScale.Z = 1.f + SpecData.Magnitude / 5.f;
-		SpectrumBar->SetWorldScale3D(FMath::VInterpTo(SpectrumBar->GetComponentScale(), BarScale, DeltaTime, InterpolationSpeed));
-	}
+  for (auto SpecData : TimeSynthComponent->GetSpectralData()) {
+    UStaticMeshComponent *SpectrumBar = SpectrumBars[SpecData.FrequencyHz / 50.f - 1];
+    FVector BarScale                  = SpectrumBar->GetComponentScale();
+    BarScale.Z                        = 1.f + SpecData.Magnitude / 5.f;
+    SpectrumBar->SetWorldScale3D(FMath::VInterpTo(SpectrumBar->GetComponentScale(), BarScale, DeltaTime, InterpolationSpeed));
+  }
 }
-
